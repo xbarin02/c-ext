43a1d6b35516bc18c592611b14380225d037c1ce
David Barina <dabler@gmail.com>
2017-12-23 01:06:21 +0100

    coding style

--- examples/do.c
+++ examples/do.c
@@ -3,9 +3,8 @@
 
 int main()
 {
+	// print all `unsigned char` values
 	do (unsigned char c = 0) {
-
 		printf("%i\n", c);
-
 	} while( ++c );
 }
--- examples/do2.cpp
+++ examples/do2.cpp
@@ -3,9 +3,8 @@
 
 int main()
 {
+	// print all `unsigned char` values
 	do (unsigned char c = 0) {
-
 		std::cout << +c << std::endl;
-
 	} while( ++c );
 }
